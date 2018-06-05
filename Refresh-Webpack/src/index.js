62e26ceb13e1ad46c953d85c9485243fe7eec0a0
MatthewBald <matt.e.bald@gmail.com>
2018-06-04 19:19:10 -0600

    bunding css & bootstrap

--- Refresh-Webpack/src/index.js
+++ Refresh-Webpack/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bootstrap';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
-  <h1>Hello world.</h1>,
+  <h1 className="h1">Hello world.</h1>,
   document.getElementById('root')
 );
--- Refresh-Webpack/webpack.config.js
+++ Refresh-Webpack/webpack.config.js
@@ -1,16 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-const htmlPlugin = new HtmlWebpackPlugin({
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: "./src/index.html",
   filename: "./index.html"
+});
+
+const providePlugin = new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default']
 });
 
 module.exports = {
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader"
+        },
+        {
+          loader: "css-loader",
+          options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: "[name]_[local]_[hash:base64]",
+            sourceMap: true,
+            minimize: true
+          }
+        }]
+      }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlWebpackPlugin, providePlugin]
 };
