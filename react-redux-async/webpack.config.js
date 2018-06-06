bfcffa43e53852af1512ecd00a7e8d148636ddba
Matthew Bald <mbald@cingroup.com>
2018-06-06 09:48:42 -0600

    create react app is not working on my work comp, using my webpack seed

---
+++ react-redux-async/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+const providePlugin = new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default']
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [{
+          loader: "style-loader"
+        },
+        {
+          loader: "css-loader",
+          options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: "[name]_[local]_[hash:base64]",
+            sourceMap: true,
+            minimize: true
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin, providePlugin]
+};
