d20b68e2bd10660ffda6932722eeda372d50a7af
Matthew Bald <mbald@cingroup.com>
2018-06-06 09:53:54 -0600

    Added initial files

--- react-redux-async/src/index.js
+++ react-redux-async/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import 'bootstrap';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <h1 className="h1">Hello world.</h1>,
+  document.getElementById('root')
+);
